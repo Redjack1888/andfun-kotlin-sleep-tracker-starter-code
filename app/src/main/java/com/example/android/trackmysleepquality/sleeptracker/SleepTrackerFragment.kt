032ec8d8dc68923e13bcec7b33aa7d375a990544
Alessandro <alessandro.mana@gmail.com>
2021-11-12 00:40:46 +0100

    Finished the RecyclerView SleepNightAdapter.kt and declared it in SleepTrackerFragment.kt to display SleepQuality Data.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.android.trackmysleepquality.R
@@ -63,6 +64,15 @@ class SleepTrackerFragment : Fragment() {
             )[SleepTrackerViewModel::class.java]
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
+
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer{
+            it?.let {
+                adapter.data = it
+            }
+        })
 
         binding.lifecycleOwner = this
 
